01acab3b6a3366d6f79e51f56444b494f09c50a7
gezer20101992 <gezer20101992@gmail.com>
2020-07-13 23:10:13 +0300

    Add Money

---
+++ Task02_KryvasheyeuYauhen/MyStore/Money.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace MyStore
+{
+    /// <summary>
+    /// support class for money
+    /// </summary>
+    public class Money
+    {
+        private int _rubles;
+        private int _coins;
+
+        public int Rubles
+        {
+            get
+            {
+                return _rubles;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("The rubles should be greater than zero");
+                _rubles = value;
+            }
+        }
+
+        public int Coins
+        {
+            get
+            {
+                return _coins;
+            }
+            set
+            {
+                if (value < 0 || value > 99)
+                    throw new ArgumentOutOfRangeException("The rubles should be greater than zero");
+                _coins = value;
+            }
+        }
+
+        public Money()
+        { }
+        public Money(int rubles, int coins)
+        {
+            Rubles = rubles;
+            Coins = coins;
+        }
+
+        public Money(Money money)
+        {
+            Rubles = money.Rubles;
+            Coins = money.Coins;
+        }
+
+        public static implicit operator int(Money money)
+        {
+            return money.Rubles * 100 + money.Coins;
+        }
+
+        public static implicit operator double(Money price)
+        {
+            return (double)(int)price / 100;
+        }
+
+        public static implicit operator Money(int intPrice)
+        {
+            int rubles = intPrice / 100;
+            int coins = intPrice % 100;
+
+            return new Money(rubles, coins);
+        }
+
+        public static explicit operator Money(double doublePrice)
+        {
+            int rubles = (int)doublePrice;
+            int coins = (int)((doublePrice - rubles) * 100);
+
+            return new Money(rubles, coins);
+        }
+
+        public static Money operator + (Money m1, Money m2)
+        {
+            int rubles = m1.Rubles + m2.Rubles;
+            int coins = m1.Coins + m2.Coins;
+            if(coins > 99)
+            {
+                rubles += 1;
+                coins -= 100;
+            }
+            return new Money(rubles, coins);
+        }
+    }
+}
---
+++ Task02_KryvasheyeuYauhen/MyStoreTest/MoneyTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyStore;
+
+namespace MoneyTest
+{
+    [TestClass]
+    public class MoneyTest
+    {
+        [TestMethod]
+        public void PropertyRublesAndCoins_WhenCorrectDataProvided_ShouldReturnCorrectData()
+        {
+            //arrange
+            Money price = new Money();
+            //act
+            price.Rubles = 2;
+            price.Coins = 34;
+            //assert
+            Assert.AreEqual($"{price.Rubles} {price.Coins}", "2 34");            
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void PropertyRubles_WhenInCorrectDataProvided_ShouldThrowExeption()
+        {
+            //arrange
+            Money price = new Money();
+            //act
+            price.Rubles = -2;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void PropertyCoins_WhenInCorrectDataProvided_ShouldThrowExeption()
+        {
+            //arrange
+            Money price = new Money();
+            //act
+            price.Coins = 100;
+        }
+
+        [TestMethod]
+        public void CastToInteger_ShouldCastCorrectle()
+        {
+            //arrange
+            Money price = new Money(2, 56);
+            //act
+            var result = (int)price;
+            //assert
+            Assert.AreEqual(result, 256);
+        }
+
+        [TestMethod]
+        public void CastToDouble_ShouldCastCorrectle()
+        {
+            //arrange
+            Money price = new Money(2, 06);
+            //act
+            var result = (double)price;
+            //assert
+            Assert.AreEqual(result, 2.06);
+        }
+
+        [TestMethod]
+        public void CastFromDouble_ShouldCastCorrectle()
+        {
+            //arrange
+            var d1 = 3.95123123;
+            //act
+            var result = (Money)d1;
+            //assert
+            Assert.AreEqual($"{result.Rubles} {result.Coins}", "3 95");
+        }
+
+        [TestMethod]
+        public void AdditionMoney_ShouldAddCorrectly()
+        {
+            //arrange
+            var m1 = new Money(3, 95);
+            var m2 = new Money(2, 7);
+            //act 
+            var result = m1 + m2;
+            //assert
+            Assert.AreEqual($"{result.Rubles} {result.Coins}", "6 2");
+        }
+
+
+    }
+}
