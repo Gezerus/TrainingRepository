82435dc492012d97a3e9a6d122028b0bd87e2e50
gezer20101992 <gezer20101992@gmail.com>
2020-07-13 23:09:33 +0300

    Add ProductTest

--- Task02_KryvasheyeuYauhen/MyStore/Product.cs
+++ Task02_KryvasheyeuYauhen/MyStore/Product.cs
@@ -1,23 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace MyStore
 {
-    public abstract class Product
+    /// <summary>
+    /// describes a product
+    /// </summary>
+    public class Product
     {
         public string Type { get; set; }
         public string Name { get; set; }
-        public Price Cost { get; set; }
+        public Money Price { get; set; }
 
-        protected Product(string name, Price cost)
-        {            
+        public Product(string type, string name, Money cost)
+        {
+            Type = type;
             Name = name;
-            Cost = new Price(cost);
+            Price = cost;
         }
+
+        public Product(Product product)
+        {
+            Type = product.Type;
+            Name = product.Name;
+            Price = product.Price;
+        }
+
+        public static Product operator + (Product p1, Product p2)
+        {
+
+            if (p1.Type != p2.Type)
+                throw new ArgumentOutOfRangeException("Products should be of the same type");
+
+            string name = p1.Name + " - " + p2.Name;
+
+            var cost = (p1.Price + p2.Price) / 2;
+
+            return new Product(p1.Type, name, cost);
+        }
+
+        public static explicit operator int(Product product)
+        {
+            return product.Price;
+        }
+
+        public static explicit operator double(Product product)
+        {
+            return product.Price;
+        }
+
+
     }
 
- 
+    
+
+
+
+
 }
